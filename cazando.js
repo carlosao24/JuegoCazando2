f976559d6261c2426c949b6b7096dd8dab5e4951
carlosao24 <carloslopez21trabajos@gmail.com>
2026-04-11 17:52:41 -0500

    Crear función detectarColision

--- cazando.js
+++ cazando.js
@@ -1,35 +1,48 @@
 let canvas = document.getElementById("juego");
 let ctx = canvas.getContext("2d");
 
-// Gato
+// GATO
 let gatoX = 0;
 let gatoY = 0;
 const ANCHOGATO = 50;
 const ALTURAGATO = 50;
 const VELOCIDAD = 15; 
 
-// Comida
+// COMIDA
 let comidaX = 50;
 let comidaY = 50;
 const ANCHOCOMIDA = 30;
 const ALTURACOMIDA = 30;
- 
+
+
+
+//FUNCION PRINCIPAL
 function graficarRectangulo(x, y, ancho, alto, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, ancho, alto);
 };
- 
+
+//FUNCION PARA GRAFICAR AL GATO
 function graficarGato() {
     graficarRectangulo(gatoX, gatoY, ANCHOGATO, ALTURAGATO, "#000000");
 };
- 
+
+//FUNCION PARA GRAFICAR COMIDA
 function graficarComida() {
     graficarRectangulo(comidaX, comidaY, ANCHOCOMIDA, ALTURACOMIDA, "#ff0000");
 };
- 
+
+
+//FUNCION PARA INICIAR EL JUEGO
 function iniciarJuego() {
+    //GATO CENTRADO
     gatoX = (canvas.width / 2) - (ANCHOGATO / 2);
     gatoY = (canvas.height / 2) - (ALTURAGATO / 2);
+
+    //UBICAR COMIDA EN ESQUINA INFERIOR
+    comidaX = canvas.width - ANCHOCOMIDA;
+    comidaY = canvas.height - ALTURACOMIDA;
+
     graficarGato();
     graficarComida();
 }
@@ -67,5 +80,12 @@ document.getElementById("btnArriba").onclick = () => moverArriba();
 document.getElementById("btnAbajo").onclick = () => moverAbajo();
 document.getElementById("btnIzquierda").onclick = () => moverIzquierda();
 document.getElementById("btnDerecha").onclick = () => moverDerecha();
- 
- 
+
+function detectarColision(){
+    if(comidaX + ANCHOCOMIDA > gatoX &&
+        comidaX < gatoX + ANCHOGATO &&
+        comidaY + ALTURACOMIDA > gatoY &&
+        comidaY < gatoY + ALTURAGATO){
+        alert("ATRAPADO!!");
+    }
+} 
